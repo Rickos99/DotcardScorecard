8654402883cf641cc857f86b66096eb1c31aab6f
Rickos99 <rickos99@hotmail.com>
2021-02-28 11:57:47 +0100

    Updated navigation pane
    - Changed visibility of hidden buttons with limited or no functionality.
    - Change players list to be populated with players in current gamemodel.
    - Added ability to load previous saved games.
    - Moved anonymous event listener functions, btn click, to named functions.

--- javascript/menu.js
+++ javascript/menu.js
@@ -1,10 +1,11 @@
 import { download, openFromFileSelector } from "./fs.js";
 import { GameModel } from "./gamemodel.js";
-import { saveToLocalStorage, findInLocalStorage, removeFromLocalStorage } from "./localstorage.js";
+import { saveToLocalStorage, findInLocalStorage, loadFromLocalStorage } from "./localstorage.js";
 
 export class Menu {
     static _navigationContainerId = "#navigation";
     static _playersContainerName = "#playernames-inputs";
+    static _oldGameKeyBegValue = "game:";
 
     /**
      * @access private
@@ -21,6 +22,12 @@ export class Menu {
         this._gameModel = gameModel;
         this.attachEventListeners();
         this.outputPreviousGames();
+        if (this._gameModel !== undefined) {
+            this._gameModel.players.forEach((playerName, index) => {
+                this.addPlayerNameInput();
+                this.setPlayerNameInputValue(playerName, index);
+            });
+        }
     }
 
     /**
@@ -63,9 +70,13 @@ export class Menu {
      * @access private
      */
     async outputPreviousGames() {
+        const gamekeyRegex = new RegExp(`^${Menu._oldGameKeyBegValue}`);
         const prevGameList = document.querySelector("#previousGamesList");
-        const entries = await findInLocalStorage(/game:.*/, false);
-        if (entries.length === 0) return;
+        const entries = await findInLocalStorage(gamekeyRegex, false);
+        if (entries.length === 0) {
+            prevGameList.innerText = "No saved games was found";
+            return;
+        }
 
         // Remove already existing list items
         while (prevGameList.firstChild) {
@@ -76,71 +87,100 @@ export class Menu {
         entries.forEach(entry => {
             const li = document.createElement("li");
             const title = document.createElement("b");
-            const link = document.createElement("a");
+            const btn = document.createElement("button");
+            const gameKey = entry.key;
 
-            title.innerText = `${entry.key}`;
-            link.innerText = "Load";
-            link.href = "#";
+            title.innerText = gameKey.replace(Menu._oldGameKeyBegValue, "");
+            btn.innerText = "Load";
+            btn.addEventListener("click", () => {
+                if (confirm("Current game will be lost, do you want to continue?")) {
+                    this.loadPreviousGame(gameKey);
+                }
+            });
 
-            li.appendChild(link);
+            li.appendChild(btn);
             li.appendChild(title);
             prevGameList.appendChild(li);
         });
+    }
+
+    loadPreviousGame(key) {
+        const prevGame = loadFromLocalStorage(key);
+        saveToLocalStorage(prevGame);
+        window.location.reload();
     }
 
     /**
      * @access private
      */
     attachEventListeners() {
-        // document.querySelector("#btnPreviousGames").addEventListener("click", () => {});
-        document.querySelector("#btnSaveGameAs").addEventListener("click", () => {
-            if (this._gameModel === undefined) {
-                alert("No game is in progress");
-                return;
-            }
+        // Disable buttons with functions that relies on a non-empty _gameModel
+        if (this._gameModel === undefined) {
+            document.querySelector("#btnSaveGameAs").disabled = true;
+            document.querySelector("#btnExportGame").disabled = true;
+        } else {
+            document.querySelector("#btnSaveGameAs").addEventListener("click", async () => this.saveGameAs());
+            document.querySelector("#btnExportGame").addEventListener("click", async () => this.exportGame());
+        }
 
-            const name = prompt("Name of game");
-            switch (name) {
-                case "currentGame":
-                    alert("Reserved name!");
-                    break;
-                case null:
-                case "":
-                    return;
-                default:
-                    break;
-            }
-
-            this._gameModel.nameOfGame = `game:${name}`;
-            saveToLocalStorage(this._gameModel);
-            alert(`Game was saved as ${name}`);
-            this.outputPreviousGames();
-        });
-        // document.querySelector("#btnNewGame").addEventListener("click", () => {});
-        document.querySelector("#btnExportGame").addEventListener("click", () => {
-            const jsonFromGamemodel = JSON.stringify(this._gameModel);
-            download(jsonFromGamemodel, this._gameModel.nameOfGame, "json");
-        });
-        document.querySelector("#btnImportGame").addEventListener("click", async () => {
-            const fileContent = await openFromFileSelector();
-            const importedGameModel = GameModel.initNew(JSON.parse(fileContent));
-            saveToLocalStorage(importedGameModel);
-            window.location.reload();
-        });
-        document.querySelector("#btnAddPlayerToList").addEventListener("click", async () => {
-            this.addPlayerNameInput();
-        });
-        document.querySelector("#btnStartGame").addEventListener("click", async () => {
-            const players = await this.getPlayerNames();
-            const diceFaces = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-            const gamemodel = new GameModel(players, diceFaces);
-            saveToLocalStorage(gamemodel);
-            window.location.reload();
-        });
+        document.querySelector("#btnImportGame").addEventListener("click", async () => this.importGame());
+        document.querySelector("#btnAddPlayerToList").addEventListener("click", async () => this.addPlayerNameInput());
+        document.querySelector("#btnStartNewGame").addEventListener("click", async () => this.startNewGame());
 
         document.querySelectorAll('[gui-role="toogle-navigation"]').forEach(el => {
             el.addEventListener("click", () => this.toggleNavigation());
         });
+    }
+
+    /**
+     * @access private
+     */
+    async importGame() {
+        const fileContent = await openFromFileSelector();
+        const importedGameModel = GameModel.initNew(JSON.parse(fileContent));
+        saveToLocalStorage(importedGameModel);
+        window.location.reload();
+    }
+
+    /**
+     * @access private
+     */
+    async exportGame() {
+        let gameName = prompt("Name of exported game:");
+        if (gameName === null) {
+            return;
+        } else if (gameName === "") {
+            gameName = "A game of DotCard";
+        }
+        const jsonFromGamemodel = JSON.stringify(this._gameModel);
+        download(jsonFromGamemodel, gameName, "json");
+    }
+
+    /**
+     * @access private
+     */
+    async saveGameAs() {
+        const name = prompt("Name of game");
+        if (name === null || name === "") {
+            return;
+        }
+
+        this._gameModel.nameOfGame = `${Menu._oldGameKeyBegValue}${name}`;
+        saveToLocalStorage(this._gameModel);
+        alert(`Game was saved as ${name}`);
+        this.outputPreviousGames();
+    }
+
+    async startNewGame() {
+        if (this._gameModel !== undefined && !confirm("Current game will be lost, do you want to continue?")) {
+            return;
+        }
+
+        const players = await this.getPlayerNames();
+        const diceFaces = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+        const gamemodel = new GameModel(players, diceFaces);
+        saveToLocalStorage(gamemodel);
+        window.location.reload();
     }
 
     /**
@@ -151,6 +191,16 @@ export class Menu {
         while (node.firstChild) {
             node.removeChild(node.lastChild);
         }
+    }
+
+    /**
+     *
+     * @param {string} playerName name of player
+     * @param {number} index
+     */
+    setPlayerNameInputValue(playerName, index) {
+        const selector = `${Menu._playersContainerName} > div > input`;
+        document.querySelectorAll(selector)[index].value = playerName;
     }
 
     /**
