fd1446a66fc6989d3e9999d879f3fb808374a2d5
Rickos99 <12218241+Rickos99@users.noreply.github.com>
2023-07-12 10:37:36 +0200

    Refactor app entry script

--- public_html/javascript/main.js
+++ public_html/javascript/main.js
@@ -1,18 +1,29 @@
 import { Menu } from "./menu.js";
-import { ScoreEntry } from "./gamemodel.js";
-import { loadFromLocalStorage, saveToLocalStorage } from "./localstorage.js";
+import { GameModel } from "./gamemodel.js";
+import { loadFromLocalStorage } from "./localstorage.js";
 import { ScoreCard } from "./scorecard.js";
 
-window.onload = () => {
+window.addEventListener("load", () => {
+    const gamemodel = loadRecentGamemodel();
+    if (gamemodel === undefined) {
+        new Menu().toggleNavigation(false);
+    } else {
+        new Menu(gamemodel);
+        const scoreCard = new ScoreCard(gamemodel);
+        scoreCard.outputModelToTable();
+    }
+});
+
+/**
+ * @returns {GameModel|undefined} gamemodel or undefined if no game is found
+ */
+function loadRecentGamemodel() {
     const gameKey = "currentGame";
     let gamemodel;
     try {
         gamemodel = loadFromLocalStorage(gameKey);
-        const scoreCard = new ScoreCard(gamemodel);
-        const menu = new Menu(gamemodel);
-        scoreCard.outputModelToTable();
     } catch (error) {
         console.log(`No game with key ${gameKey} was found`);
-        new Menu().toggleNavigation(false);
     }
-};
+    return gamemodel;
+}
